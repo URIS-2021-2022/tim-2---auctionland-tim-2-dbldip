0b169af420dda67b7729a7d4dd9d57a00ada1f52
AndrijaTasic <57434030+AndrijaTasic@users.noreply.github.com>
2022-02-25 18:16:58 +0100

    Parcela microservice (#9)
    * Initial commit
    
    * Commit
    
    Created entities
    
    * Commit
    
    Migration, finishing entities
    
    * Finished profiles, dtos, repositories, interfaces
    
    * Finshed controllers, fixing bugs
    
    * Logger implemented
    
    * Preintegration commit #1
    
    * Fixing crud operations
    
    * Swagger finished

---
+++ ParcelaWebAPI/ParcelaWebAPI/Data/CadastralMunicipalityRepository.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ParcelaWebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelaWebAPI.Data
+{
+    public class CadastralMunicipalityRepository : ICadastralMunicipalityRepository
+    {
+
+        private readonly ParcelContext context;
+        private readonly IMapper mapper;
+
+        public CadastralMunicipalityRepository(ParcelContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+        public List<CadastralMunicipality> GetCadastralMunicipalities(string nameOfCadastralMunicipality = null)
+        {
+            return context.CadastralMunicipalities.ToList();
+        }
+
+        public CadastralMunicipality GetCadastralMunicipalityById(Guid cadastralMunicipalityId)
+        {
+            return context.CadastralMunicipalities.FirstOrDefault(e => e.cadastralMunicipalityId == cadastralMunicipalityId);
+        }
+
+        public bool SaveChanges()
+        {
+            return context.SaveChanges() > 0;
+        }
+    }
+}
