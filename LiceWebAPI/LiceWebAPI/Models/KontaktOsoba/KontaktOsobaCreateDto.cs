ea49137a2db84c7ede1d7c50634de000fe70297c
Miloš Jeremić <57144161+jeremicmilos@users.noreply.github.com>
2022-02-25 18:11:27 +0100

    Lice microservice (#5)
    * Initial commit for Lice
    
    Initial commit for Lice Web API
    
    * Finished Lice Mikcroservice
    
    * Swagger finished
    
    * Swagger finished

---
+++ LiceWebAPI/LiceWebAPI/Models/KontaktOsoba/KontaktOsobaCreateDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiceWebAPI.Models.KontaktOsoba
+{
+    /// <summary>
+    /// Dto Create kontakt osoba
+    /// </summary>
+    public class KontaktOsobaCreateDto
+    {
+        /// <summary>
+        /// Ime kontakt osobae
+        /// </summary>
+        [Required(ErrorMessage = "Obavezno je uneti ime!")]
+        public string Ime { get; set; }
+
+        /// <summary>
+        /// Prezime kontakt osobe
+        /// </summary>
+        [Required(ErrorMessage = "Obavezno je uneti prezime!")]
+        public string Prezime { get; set; }
+
+        /// <summary>
+        /// Funkcija kontakt osobe
+        /// </summary>
+        [Required(ErrorMessage = "Obavezno je uneti funkciju!")]
+        public string Funkcija { get; set; }
+
+        /// <summary>
+        /// Telefon kontakt osobe
+        /// </summary>
+        [Required(ErrorMessage = "Obavezno je uneti telefon!")]
+        public string Telefon { get; set; }
+    }
+}
