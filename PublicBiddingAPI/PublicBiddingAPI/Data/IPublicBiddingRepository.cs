bc354a4e96660e39fa5555209b3d0220426f34e8
Novcic Milan <56877290+novcicmilan@users.noreply.github.com>
2022-02-25 18:08:48 +0100

    Javno nadmetanje microservice (#4)
    * Initial Commit + GetAllTypes checked
    
    * Implemented Getting Type of Public Bidding by Id
    
    Solved path too long problem when building the project and implemented get type by id
    
    * PreImplementation of Public Bidding Controller and Services
    
    Models, Profile, Entities, Updated Context
    Needs refactoring when projects are integrated!
    Change to entities when integrated (For now there are only lists of Guid attributes that mock real entities)
    
    * Post method implemented, location doesn't work, needs to implement GeyPublicBidding
    
    Get by id needs to be implemented!
    
    * Post + GetById methods implementation
    
    Implemented both getPublicBiddingById and PostMethod, tested with Postman.
    
    * Implementation of GetPublicBiddingsByAppliedUser
    
    Implemented another method in PublicBiddingRepository that returns all the public biddings that the buyer is applied to.
    
    * Implemented GetPublicBiddingsByBestBidder
    
    Implemented another method in PublicBiddingRepository that returns a list of public biddings in which the best bidder was the one who has passed Id.
    
    * Prefinal commit
    
    Should send get requests to other services, needs few new entities to represent responses.
    
    * Prefinal commit
    
    Needs implementation of DTO objects from other services after integration. Should implement sending requests to other services and and response data to the DTO which is sent back to the user.
    
    * PreIntegration Commit
    
    * Update of the update method in the repository
    
    * Working update method!!!
    
    Finally managed to implement update method so it works properly
    
    * Swagger + logger
    
    Added swagger descriptions, logger service. NEEDS AUTHENTICATION
    
    * Small change in logger

---
+++ PublicBiddingAPI/PublicBiddingAPI/Data/IPublicBiddingRepository.cs
@@ -0,0 +1,24 @@
+﻿using PublicBiddingAPI.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace PublicBiddingAPI.Data
+{
+    public interface IPublicBiddingRepository
+    {
+        List<PublicBidding> getAllPublicBiddings();
+        PublicBidding getPublicBidding(Guid biddingId);
+        List<PublicBidding> getPublicBiddingsByType(Guid typeOfPublicBidding);
+        List<PublicBidding> getPublicBiddingsByBestBidder(Guid bestBidderId);
+        List<PublicBidding> getPublicBiddingsByAppliedBuyer(Guid buyerId);
+        List<PublicBidding> getPublicBiddingsByBidder(Guid bidderId);
+        List<PublicBidding> getPublicBiddingsByCadastralMunicipality(Guid cadastralMunicipality);
+        List<PublicBidding> getPublicBiddingsByStartingPricePerHectare(double price);
+        List<PublicBidding> getPublicBiddingsByLeasePeriod(double period);
+        PublicBiddingConfirmation createPublicBidding(PublicBiddingCreation publicBidding);
+        void DeletePublicBidding(Guid publicBiddingId);
+        void UpdatePublicBidding(PublicBiddingUpdate publicBidding);
+        bool SaveChanges();
+        bool validatePublicBiddingData(PublicBiddingCreation publicBidding);
+    }
+}
---
+++ PublicBiddingAPI/PublicBiddingAPI/Entities/Bidder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicBiddingAPI.Entities
+{
+    /// <summary>
+    /// Bidder entity, used as template for a table that connects public bidding with applied bidders
+    /// </summary>
+    public class Bidder
+    {
+        [Key]
+        public Guid bidderConnectionId { get; set; }
+        /// <summary>
+        /// Public bidding in which bidders are competing
+        /// </summary>
+        public Guid publicBiddingId { get; set; }
+        /// <summary>
+        /// bidder that competes in specified public bidding
+        /// </summary>
+        public Guid bidderId { get; set; }
+    }
+}
