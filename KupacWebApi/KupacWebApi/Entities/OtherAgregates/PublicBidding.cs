203e085e4588636183bb5bfd53aad1172710b65d
AndrijaTasic <57434030+AndrijaTasic@users.noreply.github.com>
2022-02-25 18:18:02 +0100

    Kupac microservice (#10)
    * Initial commit
    
    * Finished Entities
    
    * Migration done
    
    * Finished controllers
    
    * Logger implemented
    
    * Logger finished
    
    * Changing names of requests types
    
    * Preintegration commit #1
    
    * Preintegration commit #2
    
    need to fix update method
    
    * Adding missing logger
    
    * Swagger finished
    
    * Commit
    
    * Correcting some things in swagger
    
    * Changing ports

---
+++ KupacWebApi/KupacWebApi/Entities/OtherAgregates/PublicBidding.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KupacWebApi.Entities.OtherAgregates
+{
+    [Keyless]
+
+    public class PublicBidding
+    {
+        //public Guid publicBiddingId { get; set; }
+        public DateTime date { get; set; }
+        public DateTime startingTime { get; set; }
+        public DateTime endingTime { get; set; }
+        public double bestBid { get; set; }
+        public double leasePeriod { get; set; }
+
+    }
+}
---
+++ KupacWebApi/KupacWebApi/Models/AuthorizedPersonConfirmationDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KupacWebApi.Models
+{
+    public class AuthorizedPersonConfirmationDto
+    {
+        /// <summary>
+        /// Authorized person's name
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// Authorized person's surname
+        /// </summary>
+        public string surname { get; set; }
+        /// <summary>
+        /// Authorized person's jmbg
+        /// </summary>
+        public string jmbg { get; set; }
+        /// <summary>
+        /// List of table numbers
+        /// </summary>
+        public List<int> tableNumber { get; set; }
+
+    }
+}
