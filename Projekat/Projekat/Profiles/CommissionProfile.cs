a369d3d32f4ca4aad4446db5f6c62aee476a2dd5
Nikola Bikar <76177276+karbi1@users.noreply.github.com>
2022-02-25 18:14:40 +0100

    Komisija microservice (#8)
    * Initial commit
    
    idemo
    
    * Komisija mikroservis
    
    vezbe 2
    
    * postman
    
    * context
    
    * Initial komisija push
    
    * Update
    
    * Logger implemented
    
    * Završena swagger dokumentacija

---
+++ Projekat/Projekat/Profiles/CommissionProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CommissionWebAPI.Entities;
+using CommissionWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommissionWebAPI.Profiles
+{
+    public class CommissionProfile : Profile
+    {
+        public CommissionProfile()
+        {
+            CreateMap<CommissionWithLists, CommissionDto>();
+            CreateMap<Commission, Commission>();
+
+            CreateMap<Commission, CommissionWithLists>();
+            CreateMap<Commission, CommissionDto>();
+            CreateMap<CommissionCreationDto, Commission>();
+            CreateMap<CommissionCreationDto, CommissionCreation>();
+            CreateMap<CommissionUpdateDto, Commission>();
+            CreateMap<CommissionCreation, Commission>();
+        }
+    }
+}
