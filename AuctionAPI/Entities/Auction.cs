c0ee4ba2b312c02c70a40ae75d320c2e3d818cf7
Momir Pešić <56589469+burazen-sudo@users.noreply.github.com>
2022-02-25 18:20:58 +0100

    Licitacija microservice (#2)
    * Initial commit
    
    * Added Connection Classes for public bidding
    
    * Refactoring
    
    * Refactoring + work on Repositories and Controller
    
    * Migration error in AuctionRepository, trying to fix
    
    * Migration fixed
    
    * Auction repository finished
    
    * Controller done
    
    * Quick cleanup
    
    * Code refatoring and swagger documentation comments
    
    * Small fix

---
+++ AuctionAPI/Entities/Auction.cs
@@ -0,0 +1,70 @@
+﻿
+using AuctionAPI.Entities.ManyToMany;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionAPI.Entities
+{
+    /// <summary>
+    /// Predstavlja licitaciju
+    /// </summary>
+    public class Auction
+    {
+        /// <summary>
+        /// Id licitacije
+        /// </summary>
+        [Key]
+        public Guid auctionId { get; set; }
+
+        /// <summary>
+        /// Broj licitacije
+        /// </summary>
+        public int auctionNumber { get; set; }
+
+        /// <summary>
+        /// Godina održavanja licitacije
+        /// </summary>
+        public int auctionYear { get; set; }
+        
+        /// <summary>
+        /// Datum održavanja licitacije
+        /// </summary>
+        public DateTime biddingDate { get; set; }
+
+        /// <summary>
+        /// Ograničenje
+        /// </summary>
+        public int limit { get; set; }
+        
+        /// <summary>
+        /// Korak cene
+        /// </summary>
+        public int priceStep { get; set; }
+
+        /// <summary>
+        /// Lista dokumentacije fizičkih lica
+        /// </summary>
+        [NotMapped]
+        public List<String> naturalPersonDocumentList { get; set; }
+
+        /// <summary>
+        /// Lista dokumentacije pravnih lica
+        /// </summary>
+        [NotMapped]
+        public List<String> legalPersonDocumentList { get; set; }
+        
+        /// <summary>
+        /// Lista javnih nadmetanja
+        /// </summary>
+        public List<AuctionPublicBidding> publicBiddings { get; set; }
+
+        /// <summary>
+        /// Rok za dostavljanje prijava
+        /// </summary>
+        public DateTime registryClosingDate { get; set; }
+    }
+}
