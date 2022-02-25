dcdc9455b5eb1bfd3765b35c26f797729cce789e
David Fejes <35314157+fejes99@users.noreply.github.com>
2022-02-25 19:25:40 +0100

    Prijava Microservice (#14)
    * Initial commit for project Application
    
    Initial commit for Application MS
    
    * Removed NewFolder1
    
    * Finished both Controllers
    
    CRUD for Application and Get all and by id for Priority. Next one is restrictions and exceptions
    
    * Changed port
    
    Changed port of project
    
    * Swagger done
    
    Swagger done

---
+++ Application/Application/Controllers/PriorityController.cs
@@ -0,0 +1,65 @@
+﻿using Application.Data;
+using Application.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("api/applications/priority")]
+    public class PriorityController : ControllerBase
+    {
+        private readonly IPriorityRepository priorityRepository;
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// ApplicationController constructor
+        /// </summary>
+        /// <param name="priorityRepository">Priority repository</param>
+        /// <param name="linkGenerator">Link generator</param>
+        /// <param name="mapper">AutoMapper</param>
+        public PriorityController(IPriorityRepository priorityRepository, IMapper mapper)
+        {
+            this.priorityRepository = priorityRepository;
+            this.mapper = mapper;
+        }
+
+        /// <summary>
+        /// Return all priorities
+        /// </summary>
+        /// <returns>List of priorities</returns>
+        /// <response code="200">Returns all priorities</response>
+        /// <response code="404">No priority found</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<List<PriorityDto>> GetPriorities()
+        {
+            var priorities = priorityRepository.GetPriorities();
+            return Ok(mapper.Map<List<PriorityDto>>(priorities));
+        }
+
+        /// <summary>
+        /// Returns priority by ID
+        /// </summary>
+        /// <param name="priorityId">Priority ID</param>
+        /// <returns>Priority</returns>
+        /// <response code="200">Returns priority by ID</response>
+        /// <response code="404">No priority by ID found</response>
+        [HttpGet("{priorityId}")]
+        public ActionResult<PriorityDto> GetPriority(Guid priorityId)
+        {
+            var priority = priorityRepository.GetPriorityById(priorityId);
+            if(priority == null)
+                return NoContent();
+
+            return Ok(mapper.Map<PriorityDto>(priority));
+        }
+    }
+}
---
+++ Application/Application/Data/IPriorityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Data
+{
+    public interface IPriorityRepository
+    {
+        List<Priority> GetPriorities();
+        Priority GetPriorityById(Guid priorityId);
+        bool SaveChanges();
+    }
+}
