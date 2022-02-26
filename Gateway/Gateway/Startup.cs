53e23ccdbb27ae7ac2dea719ba4d844f019a6fcf
David Fejes <david.fejes@gmail.com>
2022-02-26 20:11:18 +0100

    Gateway updated
    Wrongly pushed unfinished Gateway in last commit, fixed now

--- Gateway/Gateway/Startup.cs
+++ Gateway/Gateway/Startup.cs
@@ -1,7 +1,9 @@
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
@@ -9,12 +11,12 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Cache.CacheManager;
 using System.Text;
-using Microsoft.Extensions.Configuration;
-
 namespace Gateway
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public Startup(IConfiguration config)
         {
             Configuration = config;
@@ -22,33 +24,11 @@ namespace Gateway
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerForOcelot(Configuration);
-            services.AddSwaggerGen();
 
-            //var secret = Configuration["ApplicationSettings:JWT_Secret"].ToString();
-            //var key = Encoding.ASCII.GetBytes(secret);
-
-            //services.AddAuthentication(option =>
-            //{
-            //    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //}).AddJwtBearer(options =>
-            //{
-            //    options.RequireHttpsMetadata = false;
-            //    options.SaveToken = true;
-            //    options.TokenValidationParameters = new TokenValidationParameters
-            //    {
-            //        IssuerSigningKey = new SymmetricSecurityKey(key),
-            //        ValidateIssuerSigningKey = true,
-            //        ValidateIssuer = false,
-            //        ValidateAudience = false
-            //    };
-            //});
             services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
@@ -70,7 +50,7 @@ namespace Gateway
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Ocelot Api Gateway for AuctionLand project - Team 2");
+                    await context.Response.WriteAsync("Ocelot Api Gateway for AuctionLand project - Team 7");
                 });
             });
 
