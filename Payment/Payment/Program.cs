e492952e1b2280f8f3c3587e61f58a74c4060e34
David Fejes <35314157+fejes99@users.noreply.github.com>
2022-02-27 10:16:05 +0100

    Fejesdavid   uplata (#28)
    * Initial project struicture
    
    Initial project setup of Payment MS
    
    * Initial project structure with folders
    
    Project structure with folders
    
    * Initial commit for project Payment
    
    Initial commit for Payment MS
    
    * Test commit!!
    
    * CRUD for Payment Service
    
    Finished CRUD operations for Payment Microservice
    
    * Preintegration commit
    
    Preintegration commit, problem with delete operation, cannot delete plots in payment
    
    * Changed port
    
    Changed port of project
    
    * Preinitiation commit2
    
    Changed entities, controllers.
    
    * Preinitiation commit
    
    Swagger and logger done
    
    * Fixed warnings

--- Payment/Payment/Program.cs
+++ Payment/Payment/Program.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 
 namespace PaymentService
 {
-    public class Program
+
+    public static class Program
+
     {
         public static void Main(string[] args)
         {
